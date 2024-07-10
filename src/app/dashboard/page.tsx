ab21bad5e8d1bea8fabc84c5bafdf668fa4ed88c
Arthur Lima <arthurLM@protonmail.com>
2024-07-10 14:22:37 -0300

    sidebar

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,5 @@
 
 import { DashboardInicial } from "@/components/DashboardInicial";
-import { HeaderPage } from "@/components/HeaderPage";
-import { Login } from "@/components/Login";
-import { MenuBar } from "@/components/MenuBar";
-
 
 export default function Home() {
   return <DashboardInicial/>;
