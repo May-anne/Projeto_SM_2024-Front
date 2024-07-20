06415e0039c64b89920e00219c394e5063d92437
Arthur Lima de Araujo Miranda <60785806+ArthurLimaMiranda@users.noreply.github.com>
2024-07-19 23:14:01 -0300

    alguem me mata me examina me avalia e me treina

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -12,10 +12,6 @@ interface CardsProps {
     ehExame: boolean;
     ehTreino: boolean;
     ehAvaliacao: boolean;
-    id: string;
-    nome: string;
-    data: string;
-    info: Treino[] | Avaliacao[] | Exame[] | null;
     cpf: string | undefined;
 }
 
@@ -27,10 +23,21 @@ interface Exame {
 }
 
 interface Avaliacao {
-    id: string;
-    nome: string;
-    data: string;
-    cpf: string;
+    id: number,
+    nome: string,
+    data_nascimento: string,
+    peso: number,
+    estatura: number,
+    marcha: number,
+    cintura: number,
+    quadril: number,
+    panturrilha: number,
+    esquerdo1: number,
+    esquerdo2: number,
+    direito1: number,
+    direito2: number,
+    tug1: number,
+    tug2: number,
 }
 
 interface Treino {
@@ -46,51 +53,31 @@ interface ModalProps {
 
 export function SearchBar(props: CardsProps) {
     const [dataPublicacao, setDataPublicacao] = useState<string>('');
-    const [isExameModalOpen, setIsExameModalOpen] = useState(false);
-    const [isTreinoModalOpen, setIsTreinoModalOpen] = useState(false);
-    const [isAvaliacaoModalOpen, setIsAvaliacaoModalOpen] = useState(false);
-    const [dados, setDados] = useState<Exame[] | Avaliacao[] | Treino[]>([]);
-    const [selectedUser, setSelectedUser] = useState<Exame[] | Avaliacao[] | Treino[]>([]);
+    
+    const [dadosExame, setDadosExame] = useState<Exame[]>([]);
+    const [dadosAvaliacao, setDadosAvaliacao] = useState<Avaliacao[]>([]);
+    const [dadosTreino, setDadosTreino] = useState<Treino[]>([]);
+
 
     const handleDataChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setDataPublicacao(e.target.value);
-    };
-
-    const handlePlusButtonClick = () => {
-        if (props.ehExame) {
-            setIsExameModalOpen(true);
-        } else if (props.ehTreino) {
-            setIsTreinoModalOpen(true);
-        } else if (props.ehAvaliacao) {
-            setIsAvaliacaoModalOpen(true);
-        }
-    };
-
-    const handleCloseExame = () => {
-        setIsExameModalOpen(false);
-    };
-
-    const handleCloseTreino = () => {
-        setIsTreinoModalOpen(false);
-    };
-
-    const handleCloseAvaliacao = () => {
-        setIsAvaliacaoModalOpen(false);
     };
 
     useEffect(() => {
         let termo: 'exame' | 'treino' | 'avaliacao';
         if (props.ehExame) {
             termo = 'exame';
+            criarModalidade(termo, props.cpf).then(setDadosExame).catch(console.error);
         } else if (props.ehTreino) {
             termo = 'treino';
+            criarModalidade(termo, props.cpf).then(setDadosTreino).catch(console.error);
         } else if (props.ehAvaliacao) {
             termo = 'avaliacao';
+            criarModalidade(termo, props.cpf).then(setDadosAvaliacao).catch(console.error);
         } else {
             return;
         }
 
-        criarModalidade(termo, props.cpf).then(setDados).catch(console.error);
     }, [props.ehExame, props.ehTreino, props.ehAvaliacao, props.cpf]);
 
     return (
@@ -104,9 +91,9 @@ export function SearchBar(props: CardsProps) {
                 <button className="h-full px-4 py-2 bg-[#6B3F97] hover:bg-[#4A2569] text-white rounded-tr-md rounded-br-md">
                     Buscar
                 </button>
-                <button onClick={handlePlusButtonClick} className='rounded-md px-2 py-2 ml-2 shadow-lg bg-[#6B3F97] hover:bg-[#4A2569]'>
-                    <FaPlus className='text-white' size={20} />
-                </button>
+                    {props.ehExame&&<AddAvaliacao avaliacao={props.info} editar={false}/>}
+                    {props.ehTreino&&<AddAvaliacao avaliacao={props.info} editar={false}/>}
+                    {props.ehAvaliacao&&<AddAvaliacao avaliacao={props.info} editar={false}/>}
                 <div className="flex items-center ml-2">
                     <input
                         id="inputDataPublicacao"
@@ -129,20 +116,62 @@ export function SearchBar(props: CardsProps) {
                     </div>
                 </div>
                 <div className="overflow-y-auto h-[35vh] mt-6">
-                    {dados.map((inf) => (
-                        <div key={inf.id} className="w-[50vw] h-[5vh] bg-white items-center">
-                            <div className='flex flex-row justify-start items-center w-full'>
-                                <p className="w-[10vw] text-center border-r">{inf.id}</p>
-                                <p className="w-[15vw] text-center border-r">{inf.cpf}</p>
-                                <p className="w-[10vw] text-center border-r">{inf.data}</p>
-                                <div className="flex gap-6 items-center mx-8">
-                                    <button className="rounded-md text-red-1100 hover:bg-gray-50 px-2 py-2"><FaRegTrashAlt  /></button>
-                                    <button onClick={handlePlusButtonClick} className='text-[#6B3F97] hover:bg-gray-50 py-3 px-2 items-center flex rounded-full font-semibold'>Ver mais</button>
+                    
+                    {props.ehExame && (
+                        dadosAvaliacao.map((inf) => (
+                            <div key={inf.id} className="w-[50vw] h-[5vh] bg-white items-center">
+                                <div className='flex flex-row justify-start items-center w-full'>
+                                    <p className="w-[10vw] text-center border-r">{inf.id}</p>
+                                    <p className="w-[15vw] text-center border-r">{inf.nome}</p>
+                                    <p className="w-[10vw] text-center border-r">{inf.data}</p>
+                                    <div className="flex gap-6 items-center mx-8">
+                                        <button className="rounded-md text-red-1100 hover:bg-gray-50 px-2 py-2">
+                                            <FaRegTrashAlt />
+                                        </button>
+                                        <AddAvaliacao avaliacao={null} editar={true} />
+                                    </div>
                                 </div>
                             </div>
-                            
-                        </div>
-                    ))}
+                        ))
+                    )}
+
+                    {props.ehTreino&& (
+                        dadosExame.map((inf) => (
+                            <div key={inf.id} className="w-[50vw] h-[5vh] bg-white items-center">
+                                <div className='flex flex-row justify-start items-center w-full'>
+                                    <p className="w-[10vw] text-center border-r">{inf.id}</p>
+                                    <p className="w-[15vw] text-center border-r">{inf.nome}</p>
+                                    <p className="w-[10vw] text-center border-r">{inf.data}</p>
+                                    <div className="flex gap-6 items-center mx-8">
+                                        <button className="rounded-md text-red-1100 hover:bg-gray-50 px-2 py-2">
+                                            <FaRegTrashAlt />
+                                        </button>
+                                        <AddAvaliacao avaliacao={inf} editar={true} />
+                                    </div>
+                                </div>
+                            </div>
+                        ))
+                    )}
+
+                    {props.ehAvaliacao&& (
+                        dadosTreino.map((inf) => (
+                            <div key={inf.id} className="w-[50vw] h-[5vh] bg-white items-center">
+                                <div className='flex flex-row justify-start items-center w-full'>
+                                    <p className="w-[10vw] text-center border-r">{inf.id}</p>
+                                    <p className="w-[15vw] text-center border-r">{inf.nome}</p>
+                                    <p className="w-[10vw] text-center border-r">{inf.data}</p>
+                                    <div className="flex gap-6 items-center mx-8">
+                                        <button className="rounded-md text-red-1100 hover:bg-gray-50 px-2 py-2">
+                                            <FaRegTrashAlt />
+                                        </button>
+                                        <AddAvaliacao avaliacao={props.info} editar={true} />
+                                    </div>
+                                </div>
+                            </div>
+                        ))
+                    )}
+
+
                 </div>
             </div>
             {isExameModalOpen && (
@@ -156,13 +185,6 @@ export function SearchBar(props: CardsProps) {
                 <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
                     <div className="bg-transparent p-6 rounded-lg shadow-lg w-[60vw]">
                         <Treino isOpen={isTreinoModalOpen} onClose={handleCloseTreino} />
-                    </div>
-                </div>
-            )}
-            {isAvaliacaoModalOpen && (
-                <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
-                    <div className="bg-transparent p-6 rounded-lg shadow-lg w-[60vw]">
-                        <AddAvaliacao isOpen={isAvaliacaoModalOpen} onClose={handleCloseAvaliacao} />
                     </div>
                 </div>
             )}
