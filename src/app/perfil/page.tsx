9f7a881627686d395b27c87a78766f908fab6367
Arthur Lima de Araujo Miranda <60785806+ArthurLimaMiranda@users.noreply.github.com>
2024-07-24 09:27:07 -0300

    fix

--- src/app/perfil/page.tsx
+++ src/app/perfil/page.tsx
@@ -1,9 +1,14 @@
 import { cookies } from 'next/headers'
 import { redirect } from "next/navigation";
 import { Perfil } from "@/components/Perfil";
+import { Suspense } from "react";
 
 export default function acesso() {
-  return <Perfil />;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <Perfil />
+    </Suspense>
+  );
 }
 
 async function getData() {
